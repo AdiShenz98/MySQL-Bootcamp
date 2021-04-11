eb0dc49646428c7cfc7e8408247caecfd31addfe
Aditya Shenoy <49498445+AdiShenz98@users.noreply.github.com>
2021-04-11 10:49:42 +0530

    Add files via upload

---
+++ creating_database.sql
@@ -0,0 +1,18 @@
+DROP DATABASE cats;
+
+CREATE DATABASE bootcamp;
+
+USE bootcamp;
+
+CREATE TABLE pastries (
+	name VARCHAR(30),
+    quantity int
+);
+
+SHOW TABLES;
+
+DESC pastries;
+
+DROP TABLE pastries;
+
+SHOW TABLES;
